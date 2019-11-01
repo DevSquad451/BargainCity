98c94c99e0a785ded79664877aa63d0a690eff23
Daniel Spencer <spencerdw@vcu.edu>
2019-11-01 19:10:12 -0400

    test

---
+++ src/app/app-routing/appRoutes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './../home/home.component';
+import { DiscountedComponent } from './../discounted/discounted.component';
+import { ContactusComponent } from './../contactus/contactus.component';
+import { HelpComponent } from './../help/help.component';
+import { MyaccountComponent } from './../myaccount/myaccount.component';
+import { SearchComponent } from './../search/search.component';
+import { CartComponent } from './../cart/cart.component';
+
+export const appRoutes: Routes = [
+  { path: 'home',  component: HomeComponent },
+  { path: 'discounted',  component: DiscountedComponent },
+  { path: 'contactus',  component: ContactusComponent },
+  { path: 'help',  component: HelpComponent},
+  { path: 'myaccount',  component: MyaccountComponent},
+  { path: 'search',  component: SearchComponent},
+  { path: 'cart',  component: CartComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { DiscountedComponent } from './discounted/discounted.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HelpComponent } from './help/help.component';
+import { MyaccountComponent } from './myaccount/myaccount.component';
+import { SearchComponent } from './search/search.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DiscountedComponent,
+    ContactusComponent,
+    HelpComponent,
+    MyaccountComponent,
+    SearchComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
