a868a66f09c4d2436b93b963ce6e0df5478e0c7e
Sujit Gurung <gurungs4@vcu.edu>
2019-11-24 22:41:53 -0500

    fix

--- src/app/app-routing/appRoutes.ts
+++ src/app/app-routing/appRoutes.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './../home/home.component';
 import { DiscountedComponent } from './../discounted/discounted.component';
 import { ContactusComponent } from './../contactus/contactus.component';
 import { HelpComponent } from './../help/help.component';
-import { MyaccountComponent } from './../myaccount/myaccount.component';
 import { SearchComponent } from './../search/search.component';
 import { CartComponent } from './../cart/cart.component';
 import { TermsComponent } from './../terms/terms.component';
@@ -17,7 +16,6 @@ export const appRoutes: Routes = [
   { path: 'discounted',  component: DiscountedComponent },
   { path: 'contactus',  component: ContactusComponent },
   { path: 'help',  component: HelpComponent},
-  { path: 'myaccount',  component: MyaccountComponent},
   { path: 'search',  component: SearchComponent},
   { path: 'cart',  component: CartComponent},
   { path: 'terms',  component: TermsComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { HomeComponent } from './home/home.component';
 import { DiscountedComponent } from './discounted/discounted.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { HelpComponent } from './help/help.component';
-import { MyaccountComponent } from './myaccount/myaccount.component';
 import { SearchComponent } from './search/search.component';
 import { CartComponent } from './cart/cart.component';
 import { TermsComponent } from './terms/terms.component';
@@ -28,7 +27,6 @@ import { DetailComponent } from './detail/detail.component';
     DiscountedComponent,
     ContactusComponent,
     HelpComponent,
-    MyaccountComponent,
     SearchComponent,
     CartComponent,
     TermsComponent,
